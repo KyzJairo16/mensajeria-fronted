f06b43c4d64aeb6ced1a966926fca096eb499418
Jairo Esteban <jairodavid.estebanlozano@gmail.com>
2026-04-23 13:42:04 -0500

    SOLUCION DE ERRORES
    funcional

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { Models } from './models/models';
 import {Navbar} from './navbar/navbar';
 
 @NgModule({
-  declarations: [App, Models, Navbar],
+  declarations: [App, Navbar],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
