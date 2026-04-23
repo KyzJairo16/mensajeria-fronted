7f6c80aabd5a39d6a4b41c409965c8033651f53b
sanchezgina0312 <sanchez.gina0312@gmail.com>
2026-04-23 12:29:30 -0500

    Changes in index.html (main page)
    Changes in index.html (main page)

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navbar } from './navbar/navbar';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Navbar],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
