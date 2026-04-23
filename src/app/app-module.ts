9845ac38b9ab2816bfa6948e8356273385dce368
sanchezgina0312 <sanchez.gina0312@gmail.com>
2026-04-23 16:55:04 -0500

    Registration
    I corrected registration and also moved the main page to inicio component.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,16 +1,18 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Navbar } from './navbar/navbar';
 import { Footer } from './footer/footer';
 import { Registro } from './registro/registro';
+import { Inicio } from './inicio/inicio';
 
 @NgModule({
-  declarations: [App, Navbar, Footer, Registro],
+  declarations: [App, Navbar, Footer, Registro, Inicio],
 
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Registro } from './registro/registro';
+import { Inicio } from './inicio/inicio';
 
-const routes: Routes = [{ path: 'registro', component: Registro }];
+
+const routes: Routes = [
+  { path: '', component: Inicio },
+  { path: 'registro', component: Registro },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
