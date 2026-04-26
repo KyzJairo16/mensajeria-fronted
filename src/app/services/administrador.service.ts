37d86a62717af746a333dcd4c366b79e0564100b
LisethBuitrago <lisethramireeez@gmail.com>
2026-04-25 22:09:36 -0500

    login administrator
    logic to administrator login

--- src/app/services/administrador.service.ts
+++ src/app/services/administrador.service.ts
@@ -80,4 +80,11 @@ export class AdministradorService {
       observe: 'response',
     });
   }
+
+  loginAdmin(usuario: string, contrasenia: string) {
+    return this.administrador.post(this.urlbase + '/administrador/login?usuario=' + usuario + '&contrasenia=' + contrasenia, null, {
+      responseType: 'text'
+    });
+  }
+
 }
