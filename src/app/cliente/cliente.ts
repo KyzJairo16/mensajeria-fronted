16d7fec334179821b17f529bde26b001893293e6
LisethBuitrago <lisethramireeez@gmail.com>
2026-04-26 17:10:02 -0500

    Tarifas en sesion usuario

--- src/app/cliente/cliente.ts
+++ src/app/cliente/cliente.ts
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class Cliente implements OnInit {
   // 1. Creamos una variable para guardar los datos del usuario
   usuarioInfo: any = null;
+  tituloTarifa: string = 'Cargando tarifa...';
+  valorTarifa: string | number = '';
 
   ngOnInit(): void {
     // 2. Se ejecuta al cargar la página
@@ -22,11 +24,32 @@ export class Cliente implements OnInit {
     if (usuarioGuardado) {
       // 4. Lo convertimos en un objeto manejable por Angular
       this.usuarioInfo = JSON.parse(usuarioGuardado);
+      this.calcularTarifa();
     }
   }
   cerrarSesion() {
     // Borramos los datos del usuario logueado por seguridad
     localStorage.removeItem('usuarioLogueado');
     this.usuarioInfo = null;
+  }
+
+  calcularTarifa() {
+
+    if (this.usuarioInfo.tarifaPremium !== undefined && this.usuarioInfo.tarifaPremium !== null) {
+      this.tituloTarifa = 'Tu Tarifa Especial Premium';
+      this.valorTarifa = `15% de descuento`;
+
+    } else if (this.usuarioInfo.tarifaConcurrente !== undefined && this.usuarioInfo.tarifaConcurrente !== null) {
+      this.tituloTarifa = 'Tu Tarifa Concurrente';
+      this.valorTarifa = `5% de descuento`;
+
+    } else if (this.usuarioInfo.tarifaNormal !== undefined && this.usuarioInfo.tarifaNormal !== null) {
+      this.tituloTarifa = 'Tu Tarifa Normal';
+      this.valorTarifa = `0% de descuento`;
+
+    } else {
+      this.tituloTarifa = `No se encontro alguna tarifa para tu tipo'}`;
+      this.valorTarifa = '0% de descuento';
+    }
   }
 }
